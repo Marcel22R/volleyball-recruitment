248ec08a6fbd023703c7e0ede307b47217e874f8
Marcel Rieder <e11742405@student.tuwien.ac.at>
2022-04-09 20:23:23 +0200

    [US 0] Added bootstrap styling and header for the main page.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { PlayerListComponent } from './component/player-list/player-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PlayerListComponent,
   ],
   imports: [
     BrowserModule,
